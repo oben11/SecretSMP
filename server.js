fe61a8e731c321f83d2248080dd972f32db1028a
timmPrice <timmPrice6@gmail.com>
2025-05-14 19:20:02 -0400

    testing out some skins put into channel page

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ const __dirname = path.dirname(__filename);        // get local directory portio
 // serve html and js
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
 // get skins from usernames.txt
 function getSkins() {
     const nameFilePath = path.join(process.cwd(), 'usernames.txt')
